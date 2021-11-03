d281412072e0f6cea257b8297930ba7df1a835fa
Shihan Liu <69880398+LSH9832@users.noreply.github.com>
2021-11-03 22:25:38 +0800

    Update translation_ui2py.py

--- packages/translation_ui2py.py
+++ packages/translation_ui2py.py
@@ -60,7 +60,7 @@ class Ui_MainWindow(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">test</p></body></html>"))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p></body></html>"))
         self.floatWindowMode.setText(_translate("MainWindow", "浮窗跟随模式"))
         self.exit.setText(_translate("MainWindow", "退出"))
         self.deepColorMode.setText(_translate("MainWindow", "深色模式"))
